4931cc3899e40c0f8f7a01692ae8aa958c4a91ce
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2026-04-30 21:58:27 -0700

    Initial Commit

---
+++ trainer-crmTests/trainer_crmTests.swift
@@ -0,0 +1,19 @@
+//
+//  trainer_crmTests.swift
+//  trainer-crmTests
+//
+//  Created by Kevin Cunanan on 4/30/26.
+//
+
+import Testing
+@testable import trainer_crm
+
+struct trainer_crmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
